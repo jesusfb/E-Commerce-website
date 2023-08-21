22d53e1beec8ca7fb81aa7b99b26daa9b37194b1
kunal raj shah <rajkunal7279@gmail.com>
2023-08-21 14:55:27 +0530

    Adding env file to secure API key

--- src/LoginPage/AuthProvider.js
+++ src/LoginPage/AuthProvider.js
@@ -2,13 +2,16 @@ import React,{useState} from 'react';
 import AuthContext from './AuthContext';
 
 const AuthProvider = (props) => {
-  const[Token,setToken]=useState(null);
+  const initialToken=localStorage.getItem("Token")
+  const[Token,setToken]=useState(initialToken);
   const userLoggedIn=!!Token;
   const loginHandler=(token)=>{
     setToken(token);
+    localStorage.setItem('Token',token)
   }
   const logOutHandler=()=>{
     setToken(null);
+    localStorage.removeItem("Token")
   }
   const ContextValue={
     token:Token,
--- src/LoginPage/Login.js
+++ src/LoginPage/Login.js
@@ -1,9 +1,9 @@
-import React, { useState, useRef ,useContext,Fragment} from "react";
+import React, { useState, useRef, useContext, Fragment } from "react";
 import { useNavigate } from "react-router-dom";
 import AuthContext from "./AuthContext";
 
 const LoginPage = () => {
-  const Authctxt=useContext(AuthContext);
+  const Authctxt = useContext(AuthContext);
   const navigate = useNavigate();
   const [isLogin, setLogin] = useState(true);
   // For Loading state
@@ -17,6 +17,8 @@ const LoginPage = () => {
       setLogin(true);
     }
   };
+
+  const apiKey = process.env.REACT_APP_API_KEY; //For security purpous use api key like this.
 
   const submitHandler = async (event) => {
     event.preventDefault();
@@ -32,11 +34,9 @@ const LoginPage = () => {
     //Fetch Data
     let URL;
     if (isLogin) {
-      URL =
-        "https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyB_K-9Van1Ahl-3TdJXL88hBDtiNMebr5I";
+      URL = `https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=${apiKey}`;
     } else {
-      URL =
-        "https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyB_K-9Van1Ahl-3TdJXL88hBDtiNMebr5I";
+      URL = `https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=${apiKey}`;
     }
     try {
       const Response = await fetch(URL, {
@@ -49,7 +49,7 @@ const LoginPage = () => {
       setLoading(false);
       if (Response.ok) {
         const Data = await Response.json();
-        Authctxt.Login(Data.idToken)
+        Authctxt.Login(Data.idToken);
         navigate("/home");
         userEmail.current.value = "";
         userPassword.current.value = "";
@@ -68,71 +68,74 @@ const LoginPage = () => {
 
   return (
     <Fragment>
-    <div className="bg-gradient-to-r from-gray-800 via-gray-800 to-gray-300 flex items-center justify-center min-h-screen">
-      <div className="bg-white p-4 md:p-8 rounded-lg shadow-md w-full md:w-96">
-      <h1 className="text-center text-sm font-semibold"><span className="text-red-700">😊WELCOME</span> TO <span className="text-pink-600">BOOKMYSHOW😊</span></h1>
-        <h1 className="text-2xl font-semibold mb-4 text-center border-b-2 border-red-500">
-          {isLogin ? "Login" : "Sign up"}
-        </h1>
-        <form onSubmit={submitHandler}>
-          <div className="mb-4">
-            <label
-              for="email"
-              className="block text-sm font-semibold text-gray-700"
-            >
-              Email
-            </label>
-            <input
-              type="email"
-              id="email"
-              name="email"
-              className="mt-1 p-2 block w-full rounded-md border-gray-300"
-              ref={userEmail}
-              required
-            />
-          </div>
-          <div className="mb-4">
-            <label
-              for="password"
-              className="block text-sm font-semibold text-gray-700"
-              required
-            >
-              Password
-            </label>
-            <input
-              type="password"
-              id="password"
-              name="password"
-              className="mt-1 p-2 block w-full rounded-md border-gray-300"
-              ref={userPassword}
-            />
-          </div>
-          {!isLoading && (
-            <div className="text-center">
-              <button
-                type="Submit"
-                className="bg-blue-500 hover:bg-blue-600 text-white font-medium py-2 px-4 rounded focus:outline-none focus:ring active:bg-blue-800 w-full md:w-auto"
+      <div className="bg-gradient-to-r from-gray-800 via-gray-800 to-gray-300 flex items-center justify-center min-h-screen">
+        <div className="bg-white p-4 md:p-8 rounded-lg shadow-md w-full md:w-96">
+          <h1 className="text-center text-sm font-semibold">
+            <span className="text-red-700">😊WELCOME</span> TO{" "}
+            <span className="text-pink-600">BOOKMYSHOW😊</span>
+          </h1>
+          <h1 className="text-2xl font-semibold mb-4 text-center border-b-2 border-red-500">
+            {isLogin ? "Login" : "Sign up"}
+          </h1>
+          <form onSubmit={submitHandler}>
+            <div className="mb-4">
+              <label
+                for="email"
+                className="block text-sm font-semibold text-gray-700"
               >
-                {isLogin ? "Login" : "Create Account"}
+                Email
+              </label>
+              <input
+                type="email"
+                id="email"
+                name="email"
+                className="mt-1 p-2 block w-full rounded-md border-gray-300"
+                ref={userEmail}
+                required
+              />
+            </div>
+            <div className="mb-4">
+              <label
+                for="password"
+                className="block text-sm font-semibold text-gray-700"
+                required
+              >
+                Password
+              </label>
+              <input
+                type="password"
+                id="password"
+                name="password"
+                className="mt-1 p-2 block w-full rounded-md border-gray-300"
+                ref={userPassword}
+              />
+            </div>
+            {!isLoading && (
+              <div className="text-center">
+                <button
+                  type="Submit"
+                  className="bg-blue-500 hover:bg-blue-600 text-white font-medium py-2 px-4 rounded focus:outline-none focus:ring active:bg-blue-800 w-full md:w-auto"
+                >
+                  {isLogin ? "Login" : "Create Account"}
+                </button>
+              </div>
+            )}
+            {isLoading && (
+              <p className="text-center text-xl text-red-600 font-semibold">
+                Sending Request ....
+              </p>
+            )}
+            <div className="text-center mt-2">
+              <button
+                className="text-blue-500 border-none hover:bg-white"
+                onClick={toggleText}
+              >
+                {isLogin ? "Create new account" : "Login With existing account"}
               </button>
             </div>
-          )}
-          {isLoading && (
-            <p className="text-center text-xl text-red-600 font-semibold">
-              Sending Request ....
-            </p>
-          )}
-          <div className="text-center mt-2">
-            <button
-              className="text-blue-500 border-none hover:bg-white"
-              onClick={toggleText}
-            >
-              {isLogin ? "Create new account" : "Login With existing account"}
-            </button>
-          </div>
-        </form>
+          </form>
+        </div>
       </div>
-    </div>
     </Fragment>
   );
 };
